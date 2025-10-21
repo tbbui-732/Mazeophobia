3c642e88526a5a2d9293b31ebb576f0d9f556047
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-10-21 01:58:13 -0400

    Structure main application

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,45 @@
-#define PROJECT_NAME "Mazeophobia"
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
-int main(int argc, char **argv) {
-    std::cout << "test";
-    return 0;
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+struct Application {
+    GLFWwindow* window;
+
+    void run() {
+        init_glfw();
+        init_vulkan();
+        loop();
+        cleanup();
+    }
+
+    void init_glfw() {
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // not using opengl
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Mazeophobia - Vulkan Window", nullptr, nullptr);
+    }
+
+    void init_vulkan() {}
+
+    void loop() {}
+
+    void cleanup() {}
+};
+
+int main() {
+    Application app;
+    // TODO: avoid try-catch; use std::optional instead
+    // NOTE: what's the difference between std::optional and std::expected?
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
