ddb63a9c9e4d0bb8e6ef38f4da5f98156980c2dd
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-11-01 23:56:43 -0400

    Create GLFW handle and window; Create VkInstance

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,78 @@
-#include <iostream>
-#include <stdexcept>
-#include <cstdlib>
-
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-const uint32_t WIDTH = 800;
-const uint32_t HEIGHT = 600;
+#include <cstdint>
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <stdexcept>
 
-struct Application {
-    GLFWwindow* window;
+const std::string TITLE{"Mazeophobia"};
+const uint32_t WIDTH{1200};
+const uint32_t HEIGHT{800};
 
-    void run() {
-        init_glfw();
-        init_vulkan();
-        loop();
-        cleanup();
-    }
-
-    void init_glfw() {
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // not using opengl
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Mazeophobia - Vulkan Window", nullptr, nullptr);
-    }
-
-    void init_vulkan() {}
-
-    void loop() {}
-
-    void cleanup() {}
-};
-
-int main() {
-    Application app;
-    // TODO: avoid try-catch; use std::optional instead
-    // NOTE: what's the difference between std::optional and std::expected?
-    try {
-        app.run();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
+int main(void) {
+    // GLFW handle
+    if (glfwInit() == GLFW_FALSE) {
+        std::cerr << "RUNTIME ERR: Failed to create GLFW handle\n";
         return EXIT_FAILURE;
     }
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // not using OpenGL
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+    // GLFW window
+    constexpr auto WINDOWED_MODE        = nullptr;
+    constexpr auto DONT_SHARE_RESOURCES = nullptr;
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, TITLE.c_str(), WINDOWED_MODE, DONT_SHARE_RESOURCES);
+
+    if (window == nullptr) {
+        glfwTerminate();
+        std::cerr << "RUNTIME ERR: Failed to create GLFW window\n";
+        return EXIT_FAILURE;
+    }
+
+    // Vk Application Info
+    VkApplicationInfo appInfo{
+        VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        nullptr,
+        "Triangle",
+        VK_MAKE_VERSION(1, 0, 0),
+        "No Engine",
+        VK_MAKE_VERSION(1, 0, 0),
+        VK_API_VERSION_1_0
+    };
+
+    uint32_t glfwExtensionCount{0};
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    // Pass AppInfo to Vk handle
+    VkInstanceCreateInfo createInfo{
+        VK_STRUCTURE_TYPE_APPLICATION_INFO,          
+        nullptr,
+        0,
+        &appInfo,
+        0,
+        nullptr,
+        glfwExtensionCount,
+        glfwExtensions,
+    };
+
+    // Create Vk handle
+    VkInstance instance;
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        std::cerr << "RUNTIME ERR: Failed to create VkInstance\n";
+        return EXIT_FAILURE;
+    }
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    // Clean up
+    vkDestroyInstance(instance, nullptr);
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return EXIT_SUCCESS;
 }
