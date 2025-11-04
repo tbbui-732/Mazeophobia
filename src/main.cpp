7a3cc3c4d18d01e18c7f6f85780ff1ffb2976821
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-11-04 00:05:02 -0500

    Use base template (instance creation)

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,91 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <cstdint>
 #include <cstdlib>
-#include <exception>
 #include <iostream>
 #include <stdexcept>
 
-const std::string TITLE{"Mazeophobia"};
-const uint32_t WIDTH{1200};
-const uint32_t HEIGHT{800};
+const uint32_t WIDTH = 1920;
+const uint32_t HEIGHT = 1080;
 
-int main(void) {
-    // GLFW handle
-    if (glfwInit() == GLFW_FALSE) {
-        std::cerr << "RUNTIME ERR: Failed to create GLFW handle\n";
-        return EXIT_FAILURE;
-    }
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // not using OpenGL
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-
-    // GLFW window
-    constexpr auto WINDOWED_MODE        = nullptr;
-    constexpr auto DONT_SHARE_RESOURCES = nullptr;
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, TITLE.c_str(), WINDOWED_MODE, DONT_SHARE_RESOURCES);
-
-    if (window == nullptr) {
-        glfwTerminate();
-        std::cerr << "RUNTIME ERR: Failed to create GLFW window\n";
-        return EXIT_FAILURE;
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
     }
 
-    // Vk Application Info
-    VkApplicationInfo appInfo{
-        VK_STRUCTURE_TYPE_APPLICATION_INFO,
-        nullptr,
-        "Triangle",
-        VK_MAKE_VERSION(1, 0, 0),
-        "No Engine",
-        VK_MAKE_VERSION(1, 0, 0),
-        VK_API_VERSION_1_0
-    };
+private:
+    GLFWwindow* window;
 
-    uint32_t glfwExtensionCount{0};
-    const char** glfwExtensions;
-    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-    // Pass AppInfo to Vk handle
-    VkInstanceCreateInfo createInfo{
-        VK_STRUCTURE_TYPE_APPLICATION_INFO,          
-        nullptr,
-        0,
-        &appInfo,
-        0,
-        nullptr,
-        glfwExtensionCount,
-        glfwExtensions,
-    };
-
-    // Create Vk handle
     VkInstance instance;
-    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-        std::cerr << "RUNTIME ERR: Failed to create VkInstance\n";
+
+    void initWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+        createInstance();
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
 
-    // Main loop
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-
-    // Clean up
-    vkDestroyInstance(instance, nullptr);
-    glfwDestroyWindow(window);
-    glfwTerminate();
     return EXIT_SUCCESS;
 }
